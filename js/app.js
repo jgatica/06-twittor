756fbddb1fe9c36cdd9b365e3d40ff761b3329ab
Jorge Gatica <jorge.gatica@gmail.com>
2024-11-21 07:21:38 -0300

    Clase 76. Repaso: Configurar SW. Se registra el sw.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+if (navigator.serviceWorker) {
+    navigator.serviceWorker.register('sw.js')
+        .then(reg => console.log('Registro de SW exitoso', reg))
+        .catch(err => console.warn('Error al tratar de registrar el sw', err));
+}
 
 // Referencias de jQuery
 
